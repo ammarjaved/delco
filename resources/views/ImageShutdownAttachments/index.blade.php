5f779a884f0c70cd6a33bf1bfbf80cd30ca4086c
codewithbilalkhan <bilalkhanitsolution@gmail.com>
2024-09-04 18:16:48 +0500

    pagination on all tables

--- resources/views/ImageShutdownAttachments/index.blade.php
+++ resources/views/ImageShutdownAttachments/index.blade.php
@@ -28,8 +28,8 @@
             No files uploaded yet.
         </div>
     @else
-        <div class="table-responsive">
-            <table class="table table-bordered table-hover mt-4">
+        
+            <table id="myTable" class="table table-bordered table-hover data-table">
                 <thead class="thead-dark">
                     <tr>
                         <th scope="col">#</th>
@@ -62,7 +62,99 @@
                     @endforeach
                 </tbody>
             </table>
-        </div>
+       
     @endif
 </div>
 @endsection
+
+
+
+
+<style>
+
+    .dataTables_filter {
+        float: right !important;
+    }
+    .dataTables_paginate {
+        float: right !important;
+    }
+    .dataTables_length {
+        float: left !important;
+    }
+    .dataTables_info {
+        float: left !important;
+    }
+    
+    table.dataTable thead .sorting,
+    table.dataTable thead .sorting_asc,
+    table.dataTable thead .sorting_desc {
+        cursor: pointer;
+        position: relative;
+    }
+    
+    table.dataTable thead .sorting:after,
+    table.dataTable thead .sorting_asc:after,
+    table.dataTable thead .sorting_desc:after {
+        position: absolute;
+        bottom: 8px;
+        right: 8px;
+        display: block;
+        font-family: 'Font Awesome 5 Free';
+        opacity: 0.5;
+    }
+    
+    table.dataTable thead .sorting:after {
+        content: "\f0dc";
+        opacity: 0.2;
+    }
+    
+    table.dataTable thead .sorting_asc:after {
+        content: "\f0de";
+    }
+    
+    table.dataTable thead .sorting_desc:after {
+        content: "\f0dd";
+    }
+    
+    </style>
+
+@section('script')
+
+<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/js/generate-qr.js') }}"></script>
+
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.3/datatables.min.js"></script>
+
+<script>
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+table = $('.data-table').DataTable({
+
+    dom: '<"row"<"col-sm-6"l><"col-sm-6 text-right"f>>' +
+         '<"row"<"col-sm-12"tr>>' +
+         '<"row"<"col-sm-5"i><"col-sm-7 text-right"p>>',
+    language: {
+        search: "",
+        searchPlaceholder: "Search..."
+
+    },
+
+    // ordering: true,
+    // order: [[0, 'asc']], // Sort by the first column (index 0) in ascending order by default
+    // columnDefs: [
+    //     { orderable: false, targets: [4] } // Disable sorting on the SAT column (index 4)
+    // ]
+
+               
+               
+})
+})
+
+</script>
+
+
+@endsection
+
--- resources/views/site_survey/index.blade.php
+++ resources/views/site_survey/index.blade.php
@@ -53,6 +53,51 @@
             right: 0;
             left: auto;
         }
+
+        .dataTables_filter {
+    float: right !important;
+}
+.dataTables_paginate {
+    float: right !important;
+}
+.dataTables_length {
+    float: left !important;
+}
+.dataTables_info {
+    float: left !important;
+}
+
+table.dataTable thead .sorting,
+table.dataTable thead .sorting_asc,
+table.dataTable thead .sorting_desc {
+    cursor: pointer;
+    position: relative;
+}
+
+table.dataTable thead .sorting:after,
+table.dataTable thead .sorting_asc:after,
+table.dataTable thead .sorting_desc:after {
+    position: absolute;
+    bottom: 8px;
+    right: 8px;
+    display: block;
+    font-family: 'Font Awesome 5 Free';
+    opacity: 0.5;
+}
+
+table.dataTable thead .sorting:after {
+    content: "\f0dc";
+    opacity: 0.2;
+}
+
+table.dataTable thead .sorting_asc:after {
+    content: "\f0de";
+}
+
+table.dataTable thead .sorting_desc:after {
+    content: "\f0dd";
+}
+
     </style>
 
     
@@ -113,7 +158,7 @@
                             </div>
                             <div class="table-responsive">
     
-    <table class="table">
+    <table id="myTable" class="table table-bordered table-hover data-table">
         <thead>
             <tr>
                 <th>ID</th>
@@ -128,33 +173,36 @@
                     <td>{{ $survey->id }}</td>
                     <td>{{ $survey->nama_pe }}</td>
                     <td>{{ $survey->kawasan }}</td>
-                    <td>
-                    <div class="dropdown" style="position: absolute; z-index: 10000">
-    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-ellipsis-v"></i>
-    </button>
-    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="{{ route('site_survey.show', $survey) }}">
-            <i class="fas fa-eye"></i> Show
-        </a>
-        <a class="dropdown-item" href="{{ route('site_survey.edit', $survey) }}">
-            <i class="fas fa-edit"></i> Edit
-        </a>
-        <a class="dropdown-item" href="{{ route('siteFileUploadView.index', ['id' => $survey->id]) }}">
-            <i class="fas fa-paperclip"></i> Attach Files
-        </a>
-        <div class="dropdown-divider"></div>
-        <form action="{{ route('site_survey.destroy', $survey->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this Site Survey?');" style="display:inline;">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="dropdown-item text-danger">
-                <i class="fas fa-trash-alt"></i> Delete Site Survey
-            </button>
-        </form>
-    </div>
+                    <td class="text-center">
+                        <button type="button" class="btn  " data-toggle="dropdown">
+                            <img
+                                src="{{ URL::asset('assets/web-images/three-dots-vertical.svg') }}">
+                        </button>
+                        <div class="dropdown-menu" role="menu">
+                            <a class="dropdown-item" href="{{ route('site_survey.show', $survey) }}">
+<i class="fas fa-eye"></i> Show
+</a>
+                            <a class="dropdown-item" href="{{ route('site_survey.edit', $survey) }}">
+<i class="fas fa-edit"></i> Edit
+</a>
+<a class="dropdown-item" href="{{ route('siteFileUploadView.index', ['id' => $survey->id]) }}">
+<i class="fas fa-paperclip"></i> Attach Files
+
+
+</a>
+<div class="dropdown-divider"></div>
+<form action="{{ route('site_survey.destroy', $survey->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this Site Survey?');" style="display:inline;">
+@csrf
+@method('DELETE')
+<button type="submit" class="dropdown-item text-danger">
+<i class="fas fa-trash-alt"></i> Delete Site Survey
+</button>
+</form>
 </div>
-  
-                    </td>    
+                            
+                        </div>
+
+                    </td> 
                 </tr>
             @endforeach
         </tbody>
@@ -169,6 +217,53 @@
 
 </div>
 </div>
-</div>    
-</section>    
+</div>  
+
+
+</section> 
+
+
+
 @endsection
+
+
+@section('script')
+
+<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('assets/js/generate-qr.js') }}"></script>
+
+<script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.11.3/datatables.min.js"></script>
+
+<script>
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+table = $('.data-table').DataTable({
+
+    dom: '<"row"<"col-sm-6"l><"col-sm-6 text-right"f>>' +
+         '<"row"<"col-sm-12"tr>>' +
+         '<"row"<"col-sm-5"i><"col-sm-7 text-right"p>>',
+    language: {
+        search: "",
+        searchPlaceholder: "Search..."
+
+    },
+
+    // ordering: true,
+    // order: [[0, 'asc']], // Sort by the first column (index 0) in ascending order by default
+    // columnDefs: [
+    //     { orderable: false, targets: [4] } // Disable sorting on the SAT column (index 4)
+    // ]
+
+               
+               
+})
+})
+
+</script>
+
+
+@endsection
+
